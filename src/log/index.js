15446b9831b551d11fc385f174ed5dfbb9d4a452
Guillaume Morin <gmorin.pro@gmail.com>
2019-07-02 01:03:17 +0200

    update log step behavior

--- src/log/index.js
+++ src/log/index.js
@@ -1,80 +1,62 @@
 #!/usr/bin/env node
 
-import {setClient, displayError, getPackageJson} from '../helpers';
-import {query, mutation} from './graphql';
+import { setClient, displayError, getPackageJson } from '../helpers';
+import { mutation } from './graphql';
 import si from 'systeminformation';
 import path from 'path';
 
 const client = setClient();
-const packageJsonPath = path.join(process.cwd(), 'package.json');
+const trackedPackageJsonPath = path.join(process.cwd(), 'package.json');
 const [targetPath] = path.dirname(process.argv[1]).split('node_modules');
 const targetPackageJsonPath = path.join(targetPath, 'package.json');
 
 const getInfos = () =>
-    new Promise(resolve => {
-        const data = {};
-        return si
-            .osInfo()
-            .then(os => {
-                data.os = os;
-                return si.versions();
-            })
-            .then(versions => {
-                data.versions = versions;
-                return si.time();
-            })
-            .then(time => {
-                data.time = time;
-                return si.shell();
-            })
-            .then(() => resolve(data))
-            .catch(displayError);
-    });
+  new Promise(resolve => {
+    const data = {};
+    return si
+      .osInfo()
+      .then(os => {
+        data.os = os;
+        return si.versions();
+      })
+      .then(versions => {
+        data.versions = versions;
+        return si.time();
+      })
+      .then(time => {
+        data.time = time;
+        return si.shell();
+      })
+      .then(() => resolve(data))
+      .catch(displayError);
+  });
 
-const getPackageId = name =>
-    new Promise((resolve, reject) =>
-        client
-            .query({
-                query,
-                variables: {name}
-            })
-            .then(
-                result =>
-                    result.data.Package.id
-                        ? resolve(result.data.Package.id)
-                        : reject("Can't log. Package missing")
-            )
-            .catch(displayError)
-    );
-
-const createLog = (packageId, packageJson) => {
-    const {version} = packageJson;
-    getPackageJson(targetPackageJsonPath).then(({name: target}) =>
-        getInfos().then(data => {
-            const {os, shell, versions, time} = data;
-            client
-                .mutate({
-                    mutation,
-                    variables: {
-                        packageId,
-                        version,
-                        target,
-                        os,
-                        shell,
-                        versions,
-                        time
-                    }
-                })
-                .catch(displayError);
-        })
-    );
+const log = async (trackedPackageJson, targetPackageJson) => {
+  const { name, version } = trackedPackageJson;
+  const { name: targetName, version: targetVersion } = targetPackageJson;
+  const data = await getInfos();
+  const { os, shell, versions, time } = data;
+  client.mutate({
+    mutation,
+    variables: {
+      name,
+      version,
+      targetName,
+      targetVersion,
+      os,
+      shell,
+      versions,
+      time
+    }
+  });
 };
 
-getPackageJson(packageJsonPath)
-    .then(json => {
-        const {name} = json;
-        getPackageId(name)
-            .then(id => createLog(id, json))
-            .catch(displayError);
-    })
-    .catch(displayError);
+(async () => {
+  try {
+    const trackedPackageJson = await getPackageJson(trackedPackageJsonPath);
+    const targetPackageJson = await getPackageJson(targetPackageJsonPath);
+    log(trackedPackageJson, targetPackageJson);
+  } catch (error) {
+    displayError();
+  }
+})();
